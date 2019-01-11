1d5f6a49bce8a2fcfacdf1b5ac38bd9ab87b4b76
Karim Ebrahem <karim.ebrahem@areebgroup.com>
2019-01-11 23:02:24 +0200

    Code for exercise one in the book

---
+++ Exercise 1/exercise_one.rb
@@ -0,0 +1,9 @@
+# puts "Hello World!"
+# puts "Hello Again!"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yeah, Printing."
+# puts "I'd much rather, you 'not"
+# puts 'I "said" don not touch tihs.'
+
+puts "Karim is different. ;)"
