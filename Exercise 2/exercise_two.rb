b3f6bcf5837e9b4cddb39f4bf835050851ef548c
Karim Ebrahem <karim.ebrahem@areebgroup.com>
2019-01-11 23:08:05 +0200

    Code for exercise two Comments and pound characters

---
+++ Exercise 2/exercise_two.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read it later
+# Anything after the # is ignored by ruby
+
+puts "I could have code like this." # and code after hash is ignored.
+
+# You can comment any piece of code to disable it
+# puts "This won't run."
+
+puts "This will run."
