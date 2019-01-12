8bdfadf2cd8fa071fa195399fcec30c7a904e4eb
Karim Ebrahem <karim.ebrahem@areebgroup.com>
2019-01-12 15:15:22 +0200

    Code for exercise three Numbers and Math

---
+++ Exercise 3/exercise_three.rb
@@ -0,0 +1,37 @@
+# This line prints this statment
+puts "I will now count my chickens:"
+
+# This line prints: Hens (25 + 5) -> Hens 30
+puts "Hens #{25 + 30 / 6}"
+# This line prints: Roosters 100 - (75 % 4) -> Roosters (100 - 3) -> Roosters 97
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# This line prints this statment
+puts "Now I will count the eggs:"
+
+# This line prints: (3 + 2 + 1 - 5 + 0 - 0 + 6) -> 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This line prints this statement
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This line prints: (5 < -2) which is boolean question -> false
+puts 3 + 2 < 5 - 7
+
+# This line prints: Whay is 3 + 2? 5
+puts "What is 3 + 2? #{3 + 2}"
+# This line prints: What is 5 - 7? -2
+puts "What is 5 - 7? #{5 - 7}"
+
+# This line prints this statement
+puts "Oh, that's why it's false."
+
+# This line prints this statement
+puts "How about some more."
+
+# This line prints: Is it greater? true
+puts "Is it greater? #{5 > -2}"
+# This line prints: Is it greater or equal? true
+puts "Is it greater or equal? #{5 >= -2}"
+# This line prints: Is it less or equal? false
+puts "Is it less or equal? #{5 <= -2}"
---
+++ Exercise 3/exercise_three_floating_points.rb
@@ -0,0 +1,37 @@
+# This line prints this statment
+puts "I will now count my chickens:"
+
+# This line prints: Hens (25 + 5) -> Hens 30
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# This line prints: Roosters 100 - (75 % 4) -> Roosters (100 - 3) -> Roosters 97
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# This line prints this statment
+puts "Now I will count the eggs:"
+
+# This line prints: (3 + 2 + 1 - 5 + 0 - 0 + 6) -> 7
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This line prints this statement
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# This line prints: (5 < -2) which is boolean question -> false
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# This line prints: Whay is 3 + 2? 5
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# This line prints: What is 5 - 7? -2
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# This line prints this statement
+puts "Oh, that's why it's false."
+
+# This line prints this statement
+puts "How about some more."
+
+# This line prints: Is it greater? true
+puts "Is it greater? #{5.0 > -2.0}"
+# This line prints: Is it greater or equal? true
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# This line prints: Is it less or equal? false
+puts "Is it less or equal? #{5.0 <= -2.0}"
